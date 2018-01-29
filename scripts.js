bc134e356b89b6c1ea9f968b13686067a76c7ade
Jennah <contactjennah@gmail.com>
2018-01-29 18:58:42 +0000

    Create canvas

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener("DOMContentLoaded", function() {
+  //set up the canvas
+  var canvas = document.querySelector('#canvas');
+  var ctx = canvas.getContext('2d');
+
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 4;
+
   
 });
