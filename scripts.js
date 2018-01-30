0d2a278eec4d4245df0d444e8f825dded71ef4ce
Jennah <contactjennah@gmail.com>
2018-01-30 16:03:20 +0000

    Amend whereClicked() and makeMove() so that they update global board array

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 //global variables
 var playerTurn = 'X';
+var board = [[],[],[]];
 
 document.addEventListener("DOMContentLoaded", function() {
   //set up the canvas
@@ -43,40 +44,55 @@ document.addEventListener("DOMContentLoaded", function() {
 
 /* FUNCTIONS */
 
-//determines where the user clicked on the canvas and set the x and y values accordingly
+/*
+  whereCLicked
+  determines where the user clicked on the canvas and set the x and y values accordingly
+  also uses the new x and y to determine how the board array should be updated
+*/
 function whereClicked(ctx) {
   var x = event.clientX - canvas.offsetLeft;
   var y = event.clientY - canvas.offsetTop;
 
+  //variables to determine array indexes of board array to update
+  var boardX, boardY;
+
   if(x >=0 && x <= 150) {
     x = 55;
+    boardX = 0;
   } else if(x >= 151 && x <= 300) {
     x = 205;
+    boardX = 1;
   } else {
     x = 355;
+    boardX = 2;
   }
 
   if(y >=0 && y <= 150) {
     y = 95;
+    boardY = 0;
   } else if(y >= 151 && y <= 300) {
     y = 250;
+    boardY = 1;
   } else {
     y = 395;
+    boardY = 2;
   }
 
-  makeMove(x, y, ctx);
+  makeMove(x, y, ctx, boardX, boardY);
 }
 
-//Draws an X or O to canvas depending on whose turn it is
-function makeMove(x, y, ctx) {
+//Draws an X or O to canvas depending on whose turn it is & updates board array
+function makeMove(x, y, ctx, boardX, boardY) {
   ctx.font = '60px helvetica';
 
   if(playerTurn == 'X') {
     ctx.fillText('X', x, y);
+    board[boardY][boardX] = 'X';
     playerTurn = 'O';
     document.querySelector('#turn').innerHTML = 'O\'s ';
   } else {
     ctx.fillText('O', x, y);
+    board[boardY][boardX] = 'O';
     playerTurn = 'X';
     document.querySelector('#turn').innerHTML = 'X\'s ';
   }
