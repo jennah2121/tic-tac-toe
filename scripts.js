e6c5d66fa66b777048cd10c01ba06972bfd669ae
Jennah <contactjennah@gmail.com>
2018-01-30 15:09:00 +0000

    Add click event listener to canvas and function to determine where user clicked

--- scripts.js
+++ scripts.js
@@ -31,4 +31,33 @@ document.addEventListener("DOMContentLoaded", function() {
   ctx.lineTo(450, 300);
   ctx.stroke();
   ctx.closePath();
+
+  //add a click event listener to canvas
+  canvas.addEventListener('click', function() {
+    whereClicked(ctx);
+  });
 });
+
+/* FUNCTIONS */
+
+//determines where the user clicked on the canvas and set the x and y values accordingly
+function whereClicked(ctx) {
+  var x = event.clientX - canvas.offsetLeft;
+  var y = event.clientY - canvas.offsetTop;
+
+  if(x >=0 && x <= 150) {
+    x = 55;
+  } else if(x >= 151 && x <= 300) {
+    x = 205;
+  } else {
+    x = 355;
+  }
+
+  if(y >=0 && y <= 150) {
+    y = 95;
+  } else if(y >= 151 && y <= 300) {
+    y = 250;
+  } else {
+    y = 395;
+  }
+}
